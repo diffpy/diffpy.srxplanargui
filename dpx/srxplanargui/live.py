4ce8d33c70ed5c5f417936dde747f06445b42c21
XiaohaoYang <sodestiny1@gmail.com>
2013-10-11 04:46:53 +0000

    update srxgui

--- dpx/srxplanargui/live.py
+++ dpx/srxplanargui/live.py
@@ -1,101 +1,41 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# dpx.pdfgetxgui    by Simon J. L. Billinge group
-#                   (c) 2012 Trustees of the Columbia University
+# diffpy.srxplanar  by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2010 Trustees of the Columbia University
 #                   in the City of New York.  All rights reserved.
 #
 # File coded by:    Xiaohao Yang
 #
 # See AUTHORS.txt for a list of people who contributed.
-# See LICENSE.txt for license information.
+# See LICENSENOTICE.txt for license information.
 #
 ##############################################################################
 
-'''provide UI for srxplanar
-'''
-
 import numpy as np
-import os
-import sys
+import re, os, sys
+from functools import partial
 
 from traits.api import \
     Dict, List, Enum, Bool, File, Float, Int, Array, Str, Range, Directory, CFloat, CInt,\
     HasTraits, Property, Instance, Event, Button, Any, \
     on_trait_change, DelegatesTo, cached_property, property_depends_on
-
+    
 from traitsui.api import \
     Item, Group, View, Handler, Controller, spring, Action, \
     HGroup, VGroup, Tabbed, \
     RangeEditor, CheckListEditor, TextEditor, EnumEditor, ButtonEditor, \
-    ArrayEditor, TitleEditor, TableEditor, HistoryEditor, InstanceEditor, ImageEditor
+    ArrayEditor, TitleEditor, TableEditor, HistoryEditor, InstanceEditor, BooleanEditor
 from traitsui.menu import ToolBar, OKButton, CancelButton, Menu, MenuBar, OKCancelButtons
 from pyface.api import ImageResource
 from enthought.pyface.api import SplashScreen
 
 from dpx.srxplanargui.selectfiles import AddFiles
 from dpx.srxplanargui.srxconfig import SrXconfig
-from dpx.srxplanar.srxplanar import SrXplanar
+from dpx.srxplanargui.srxgui import SrXgui, SrXguiHandler
 
-class SrXguiHandler(Handler):
-    
-    def closed(self, info, is_ok):
-        '''
-        notify main gui to delete current plot in plots list
-        '''
-        info.object.saveConfig('default')
-        return True
-    
-    def _aboutmgs(self, info):
-        info.object.edit_traits(view = "about_view")
-        return
-    
-class SrXgui(HasTraits):
-    
-    addfiles = Instance(AddFiles)
-    srxconfig = Instance(SrXconfig)
-    splash = Any
-    
-    def saveConfig(self, filename=None):
-        '''
-        save config
-        '''
-        configfile = os.path.join(os.path.expanduser('~'), 'srxconfig.cfg')
-        if filename == None:
-            if os.path.exists(self.srxconfig.configfile): 
-                configfile = self.srxconfig.configfile
-        else:
-            if filename != 'default':
-                configfile = filename
-        self.srxconfig.writeConfig(configfile, mode='full')
-        return
-    
-    def loadConfig(self, filename=None):
-        '''
-        load config
-        '''
-        configfile = os.path.join(os.path.expanduser('~'), 'srxconfig.cfg')
-        if filename == None:
-            if os.path.exists(self.srxconfig.configfile): 
-                configfile = self.srxconfig.configfile
-        else:
-            if os.path.exists(filename):
-                configfile = filename
-        
-        if os.path.exists(configfile):
-            self.srxconfig.updateConfig(filename = configfile)
-        return
-    
-    def processSelected(self, ss=False):
-        if self.addfiles.selected:
-            self.srx.updateConfig()
-            filelist = [f.fullname for f in self.addfiles.selected]
-            self.srx.prepareCalculation(filelist)
-            if ss:
-                self.srx.integrateFilelist(filelist, summation=True)
-            else:
-                self.srx.integrateFilelist(filelist, summation=False)
-        return
+class SrXguiLive(SrXgui):
     
     def __init__(self, configfile=None, args=None, **kwargs):
         '''
@@ -111,27 +51,22 @@ class SrXgui(HasTraits):
         self.loadConfig('default')
         self.splash.close()
         return
-    
-    def _integratbb_changed(self):
-        self.processSelected(False)
+
+    def _startlivebb_changed(self):
+        self.capturing = True
         return
     
-    def _integratessbb_changed(self):
-        self.processSelected(True)
+    def _stoplivebb_changed(self):
+        self.capturing = False
         return
     
-    def _aboutbb_changed(self):
-        self.edit_traits(view = 'about_view')
-        return
+    capturing = Bool(False)
+    startlivebb = Button('Start capturing')
+    stoplivebb = Button('Stop capturing')
     
     about_action = \
         Action(name = 'About',
-               action = '_aboutmgs')
-    
-    integratbb = Button('Integrate separately')
-    integratssbb = Button('Sum and Integrate')
-    aboutbb = Button('About')
-    
+               action = '_aboutmgs')  
     main_group = \
         HGroup(Item('addfiles', editor = InstanceEditor(view = 'traits_view'),
                     style = 'custom', label = 'Files', width=0.4),
@@ -147,17 +82,21 @@ class SrXgui(HasTraits):
                dock = 'tab',
                show_labels = False
                )
-    
+
     traits_view = \
         View(Group(main_group,
                    HGroup(spring,
                           Item('integratbb'),
                           Item('integratssbb'),
-                          Item('aboutbb'),
                           spring,
+                          Item('startlivebb', enabled_when='capturing == False'),
+                          Item('stoplivebb', enabled_when='capturing == True'),
+                          spring,
+                         
                           show_labels = False,
                           ),
                    ),
+             
              resizable=True, 
              title='SrXgui',
              width=800, 
@@ -165,27 +104,17 @@ class SrXgui(HasTraits):
              kind = 'live',
              icon = ImageResource('icon.ico'),
              handler = SrXguiHandler(),
+             buttons = [OKButton, about_action],
              )
-    
-    qsimage = ImageResource('splash.png')
-    
-    about_view = \
-        View(Item('qsimage', editor = ImageEditor(), width = 0.5, show_label=False),
-             title = 'Quick start',
-             width = 960,
-             height = 520, 
-             resizable = True,
-             buttons = [OKButton],
-             icon = ImageResource('icon.ico'),
-             )
-            
+
 def main():
     splash = SplashScreen(image=ImageResource('splash.png'))
     splash.open()
     
-    gui = SrXgui(splash=splash)
+    gui = SrXguiLive(splash=splash)
     gui.configure_traits(view = 'traits_view')
     return
 
 if __name__=='__main__':
     sys.exit(main())
+    
