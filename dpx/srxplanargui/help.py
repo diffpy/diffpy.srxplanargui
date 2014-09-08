a9bc8be4e6f9bd8e64c1969c678f654c0ce148cc
XiaohaoYang <sodestiny1@gmail.com>
2014-09-08 16:33:28 -0400

    bug fix: manual pages
    Former-commit-id: 00e8659af10fd2cb5f23669e5a240af513d5c22d

--- dpx/srxplanargui/help.py
+++ dpx/srxplanargui/help.py
@@ -64,7 +64,7 @@ class SrXguiHelp(HasTraits):
 
     imgs = [ImageResource('%02d.png' % i) for i in range(1, 14)]
 
-    qslen = Int(1)
+    qslen = Int(12)
 
     next_action = \
         Action(name='Next',
