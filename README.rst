f14fac010f2326d1fe97e4961ceb7ad2272a85ab
XiaohaoYang <sodestiny1@gmail.com>
2016-04-09 20:54:40 -0400

    update setup script

--- README.rst
+++ README.rst
@@ -10,56 +10,30 @@ The dpx.srxplanargui requires Python 2.7 and the following software:
 
 * ``diffpy.srxplanar``
 * ``dpx.confutils``
-* ``dpx.srxplanargui``
 * ``numpy``
+* ``scipy``
 * ``traits``
 * ``traitsui``
 * ``chaco``
-* ``mayavi``
 
-To enable calibrant image calibration function, following packages are required:
+The image integration (diffpy.srxplanargui) requires
 
-* ``pyFAI``
-* ``FabIO``
+* ``fabio``
+* ``pyfai``
 * ``matplotlib``
-
-BACKENDS
-------------------------------------------------------------------------
-
-SrXplanargui can use both WX and Qt backends. Please note that traitsui has
-compatibility issuses with WX>2.8. It only works with WX 2.8. So it is strongly
-recommonded to use QT backend (default). Especailly for MacOSX which the WX2.8 is not
-available for the latest distribution.
-
-WX backend requires:
-* ``wxpython=2.8``
-
-Qt backend requires:
-* ``pyQt``
-or
-* ``pyside``
-
-If you would like to use image calibration function in pyFAI, please note that
-pyside may not work and you probably need to switch to pyQt.
-
-To start SrXplanargui using different backend, use
-
-    srxplanargui --toolkit backend
-
-where backend could be "wx" or "qt"
 
 INSTALLATION
 ------------------------------------------------------------------------
 
-To install the dpx.pdfgetxgui package:
+We are going to release conda package for all platform. For general user
+please use the installation file and install software. For developor, 
+you can install dpx.srxplanargui using
 
     python setup.py install
+    
+Note: the dependency is not specified in the setup.py. You need to install 
+them yourself. You can use Anaconda or other python enviroment.    
 
-By default the files are installed in the system directories, which are
-usually only writeable by the root.  See the usage info "./setup.py install
---help" for options to install as a normal user under a different location.
-Note that installation to non-standard directories you may require adjustments
-to the PATH and PYTHONPATH environment variables.
 
 CONTACTS
 ------------------------------------------------------------------------
--- setup.py
+++ setup.py
@@ -10,52 +10,70 @@ Scripts:    srxgui
 """
 
 import os
-import glob
 from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+import py_compile
+
+
+class custom_build_pyc(build_py):
+
+    def byte_compile(self, files):
+        for file in files:
+            if file.endswith('.py'):
+                py_compile.compile(file)
+                os.unlink(file)
+
+# Use this version when git data are not available, like in git zip archive.
+# Update when tagging a new release.
+FALLBACK_VERSION = '1.0'
 
 # versioncfgfile holds version data for git commit hash and date.
 # It must reside in the same directory as version.py.
 MYDIR = os.path.dirname(os.path.abspath(__file__))
 versioncfgfile = os.path.join(MYDIR, 'dpx', 'srxplanargui', 'version.cfg')
-defaultversion = '1.0'
+gitarchivecfgfile = versioncfgfile.replace('version.cfg', 'gitarchive.cfg')
+
 
 def gitinfo():
     from subprocess import Popen, PIPE
     kw = dict(stdout=PIPE, cwd=MYDIR)
-    rv = {}
     proc = Popen(['git', 'describe', '--match=v[[:digit:]]*'], **kw)
     desc = proc.stdout.read()
     proc = Popen(['git', 'log', '-1', '--format=%H %at %ai'], **kw)
     glog = proc.stdout.read()
-    if desc != '':
-        rv['version'] = desc.strip().split('-')[0].lstrip('v')
-    else:
-        rv['version'] = defaultversion
-    if glog != '':
-        rv['commit'], rv['timestamp'], rv['date'] = glog.strip().split(None, 2)
-    else:
-        rv['commit'], rv['timestamp'], rv['date'] = 'no git', 'no git', 'no git'
+    rv = {}
+    rv['version'] = '.post'.join(desc.strip().split('-')[:2]).lstrip('v')
+    rv['commit'], rv['timestamp'], rv['date'] = glog.strip().split(None, 2)
     return rv
 
 
 def getversioncfg():
-    from ConfigParser import SafeConfigParser
-    cp = SafeConfigParser()
-    cp.read(versioncfgfile)
+    import re
+    from ConfigParser import RawConfigParser
+    vd0 = dict(version=FALLBACK_VERSION, commit='', date='', timestamp=0)
+    # first fetch data from gitarchivecfgfile, ignore if it is unexpanded
+    g = vd0.copy()
+    cp0 = RawConfigParser(vd0)
+    cp0.read(gitarchivecfgfile)
+    if '$Format:' not in cp0.get('DEFAULT', 'commit'):
+        g = cp0.defaults()
+        mx = re.search(r'\btag: v(\d[^,]*)', g.pop('refnames'))
+        if mx:
+            g['version'] = mx.group(1)
+    # then try to obtain version data from git.
     gitdir = os.path.join(MYDIR, '.git')
-    if not os.path.isdir(gitdir):
-        # not a git repo
-        cp.set('DEFAULT', 'version', defaultversion)
-        cp.set('DEFAULT', 'commit', 'no git')
-        cp.set('DEFAULT', 'date', 'no git')
-        cp.set('DEFAULT', 'timestamp', 'no git')
-        cp.write(open(versioncfgfile, 'w'))
-    try:
-        g = gitinfo()
-    except OSError:
-        return cp
+    if os.path.exists(gitdir) or 'GIT_DIR' in os.environ:
+        try:
+            g = gitinfo()
+        except OSError:
+            pass
+    # finally, check and update the active version file
+    cp = RawConfigParser()
+    cp.read(versioncfgfile)
     d = cp.defaults()
-    if g['version'] != d.get('version') or g['commit'] != d.get('commit'):
+    rewrite = not d or (g['commit'] and (
+        g['version'] != d.get('version') or g['commit'] != d.get('commit')))
+    if rewrite:
         cp.set('DEFAULT', 'version', g['version'])
         cp.set('DEFAULT', 'commit', g['commit'])
         cp.set('DEFAULT', 'date', g['date'])
@@ -65,29 +83,38 @@ def getversioncfg():
 
 versiondata = getversioncfg()
 
+
+def dirglob(d, *patterns):
+    from glob import glob
+    rv = []
+    for p in patterns:
+        rv += glob(os.path.join(d, p))
+    return rv
+
 # define distribution
 setup_args = dict(
-        name='dpx.srxplanargui',
-        version=versiondata.get('DEFAULT', 'version'),
-        namespace_packages=['dpx'],
-        packages=find_packages(),
-        include_package_data=True,
-        zip_safe=False,
-        entry_points={
-            # define console_scripts here, see setuptools docs for details.
-            'console_scripts' : ['srxgui = dpx.srxplanargui.srxguiapp:main',
-                                 ],
-        },
+    name='dpx.srxplanargui',
+    cmdclass=dict(build_py=custom_build_pyc),
+    version=versiondata.get('DEFAULT', 'version'),
+    namespace_packages=['dpx'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        # define console_scripts here, see setuptools docs for details.
+        'console_scripts': ['srxgui = dpx.srxplanargui.srxguiapp:main',
+                            ],
+    },
 
-        author='Simon J.L. Billinge',
-        author_email='sb2896@columbia.edu',
-        description='PDFgetXgui, a software for PDF transformation and visualization',
-        maintainer='Xiaohao Yang',
-        maintainer_email='xiaohao.yang@outlook.com',
-        license='see LICENSENOTICE.txt',
-        url='',
-        keywords='2D powder diffraction image integration uncertainty propagation',
-        classifiers=[
+    author='Simon J.L. Billinge',
+    author_email='sb2896@columbia.edu',
+    description='xPDFsuite, a software for PDF transformation and visualization',
+    maintainer='Xiaohao Yang',
+    maintainer_email='sodestiny1@gmail.com',
+    license='see LICENSENOTICE.txt',
+    url='',
+    keywords='2D powder diffraction image integration uncertainty propagation',
+    classifiers=[
             # List of possible values at
             # http://pypi.python.org/pypi?:action=list_classifiers
             'Development Status :: 5 - Production/Stable',
@@ -101,7 +128,7 @@ setup_args = dict(
             'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
             'Topic :: Scientific/Engineering :: Physics',
-        ],
+    ],
 )
 
 if __name__ == '__main__':
