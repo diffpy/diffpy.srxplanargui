22c82cfa16bed9def7e6572c09a6e0b788608bb7
stevenhua0320 <r.hua@mail.utoronto.ca>
2025-10-07 10:15:38 -0400

    skpkg: modify license time and author in py file

--- src/diffpy/srxplanargui/version.py
+++ src/diffpy/srxplanargui/version.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# (c) 2025 The Trustees of Columbia University in the City of New York.
+# (c) 2012-2025 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
-# File coded by: Rundong Hua, Simon Billinge, Billinge Group members.
+# File coded by: Xiaohao Yang, Simon Billinge, Billinge Group members.
 #
 # See GitHub contributions for a more detailed list of contributors.
 # https://github.com/diffpy/diffpy.srxplanargui/graphs/contributors  # noqa: E501
