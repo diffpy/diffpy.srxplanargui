3b68bab0d41185bd0e74ac373d5fba9a05f171e8
stevenhua0320 <r.hua@mail.utoronto.ca>
2025-10-07 16:17:23 -0400

    skpkg: change author and contributor

--- src/diffpy/srxplanargui/__init__.py
+++ src/diffpy/srxplanargui/__init__.py
@@ -4,7 +4,7 @@
 # (c) 2012-2025 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
-# File coded by: Xiaohao Yang, Simon Billinge, Billinge Group members.
+# File coded by: Xiaohao Yang, Billinge Group members.
 #
 # See GitHub contributions for a more detailed list of contributors.
 # https://github.com/diffpy/diffpy.srxplanargui/graphs/contributors
