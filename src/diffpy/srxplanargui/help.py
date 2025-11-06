73520d271ce75f648a722a15aa12138bb9500a7d
Zhi Ming Xu <zx2525@columbia.edu>
2025-11-06 00:12:11 -0500

    skpkg: update readme, license, delete instructions.txt, update module level comments

--- src/diffpy/srxplanargui/help.py
+++ src/diffpy/srxplanargui/help.py
@@ -7,8 +7,8 @@
 #
 # File coded by:    Xiaohao Yang
 #
-# See AUTHORS.txt for a list of people who contributed.
-# See LICENSE.txt for license information.
+# See AUTHORS.rst for a list of people who contributed.
+# See LICENSE.rst for license information.
 #
 ##############################################################################
 """Provide help for SrXgui."""
